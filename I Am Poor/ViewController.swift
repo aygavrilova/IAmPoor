d77354ea427773b4388aa3300a45aa6955280c86
Anna Gavrilova <agavrilova@Annas-MacBook-Pro.local>
2020-08-25 15:01:43 -0400

    first commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Anna Gavrilova on 2020-08-17.
+//  Copyright © 2020 Anna Gavrilova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
